f795f0068f0eee6897ad590af561828eebd79ec4
patrickhess <patrickhess@patrickhess-macbookpro.roam.corp.google.com>
2016-02-10 00:05:03 -0800

    Add logic to read test file, and predict top 5 countries

--- airbnb4.py
+++ airbnb4.py
@@ -65,6 +65,8 @@ for it, row in raw_train.iterrows():
                   'affiliate_provider': 'unknown'  if (str(row['affiliate_provider']) == 'nan') else row['affiliate_provider'],
                   'first_affiliate_tracked': 'unknown' if (str(row['first_affiliate_tracked']) == 'nan') else row['first_affiliate_tracked'],
                   'signup_app': 'unknown' if (str(row['signup_app']) == 'nan') else row['signup_app'],
+                  'first_device_type': 'unknown' if (str(row['first_device_type']) == 'nan') else row['first_device_type'],
+                  'first_browser': 'unknown' if (str(row['first_browser']) == 'nan') else row['first_browser'],
                   'month_first_booking': 'unknown' if (str(row['date_first_booking']) == 'nan') else str(parse(row['date_first_booking']).month) 
                   }
     X.append(data_point)
@@ -100,16 +102,59 @@ def build_model(input_dim, output_dim, hn=64, dp=0.5, layers=1):
     model.compile(loss='categorical_crossentropy', optimizer='adam')
     return model
 
-EPOCHS = 4
+EPOCHS = 16
 BATCHES = 128
-HN = 64
+HN = 512
 RUN_FOLDS = False
 nb_folds = 4
 kfolds = KFold(len(y), nb_folds)
 av_ll = 0.
 f = 0
+LAYERS  = 2
+dp = 0.5
 
 print("Generating submission...")
 
-model = build_model(input_dim, output_dim, HN)
-model.fit(X, Y, nb_epoch=EPOCHS, batch_size=BATCHES, verbose=0) 
+model = build_model(input_dim, output_dim, HN, dp, LAYERS)
+model.fit(X, Y, nb_epoch=EPOCHS, batch_size=BATCHES, verbose=0)
+
+raw_train = pandas.read_csv("/Users/patrickhess/Downloads/test_users.csv")
+
+Xtest = []
+ids = []
+for it, row in raw_train.iterrows():
+    data_point = {
+                  'gender': 'unknown' if (str(row['gender']) == 'nan') else row['gender'],
+                  'age': 'unknown' if (str(row['age']) == 'nan') else row['age'],
+                  'signup_method': 'unknown' if (str(row['signup_method']) == 'nan') else row['signup_method'],
+                  'signup_flow': 'unknown' if (str(row['signup_flow']) == 'nan') else row['signup_flow'],
+                  'language': 'unknown' if (str(row['language']) == 'nan') else row['language'],
+                  'affiliate_channel': 'unknown'  if (str(row['affiliate_channel']) == 'nan') else row['affiliate_channel'],
+                  'affiliate_provider': 'unknown'  if (str(row['affiliate_provider']) == 'nan') else row['affiliate_provider'],
+                  'first_affiliate_tracked': 'unknown' if (str(row['first_affiliate_tracked']) == 'nan') else row['first_affiliate_tracked'],
+                  'signup_app': 'unknown' if (str(row['signup_app']) == 'nan') else row['signup_app'],
+                  'first_device_type': 'unknown' if (str(row['first_device_type']) == 'nan') else row['first_device_type'],
+                  'first_browser': 'unknown' if (str(row['first_browser']) == 'nan') else row['first_browser'],
+                  'month_first_booking': 'unknown' if (str(row['date_first_booking']) == 'nan') else str(parse(row['date_first_booking']).month) 
+                  }
+    ids.append(row['id'])
+    Xtest.append(data_point)
+
+Xtest = dctX.transform(Xtest)
+Ypred = model.predict(Xtest)
+output = []
+
+countries = [c.split("=")[1] for c in dctY.get_feature_names()]
+
+with gzip.open('airbnb.csv.gz', 'wt') as outf:
+  fo = csv.writer(outf, lineterminator='\n')
+  fo.writerow(['Id'] + ['Country'])
+  pos = 0
+  for pos in range(len(Ypred)):
+    pred = Ypred[pos]
+    sorted_indexes = numpy.argsort(pred)[::-1][:5]
+    idd = ids[pos]
+    for i in range(5):
+      fo.writerow([idd] + [countries[sorted_indexes[i]]])
+        
+
